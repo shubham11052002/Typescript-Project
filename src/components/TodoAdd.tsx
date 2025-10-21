9753cb941a649c60a14f5ace58423fdd7ed7fc93
shubhamsharma21505 <shubham.s@tekanthem.com>
2025-10-21 21:55:57 +0530

    add error or empty status

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -5,11 +5,20 @@ import { useTodos } from "@/store/todos";
 const TodoAdd = () => {
   const [todo, setTodo] = useState("");
   const { handleAddTodo } = useTodos();
-
+  const [error, setError] = useState<string>();
   const handleFSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!todo.trim()) {
+      setError("Todo cannot be empty!"); 
+      return;
+    }
+
     handleAddTodo(todo);
     setTodo("");
+  };
+  const handleInputChange = (value: string) => {
+    setTodo(value);
+    if (value.trim()) setError("");
   };
 
   return (
@@ -20,11 +29,12 @@ const TodoAdd = () => {
           placeholder="Enter your todo"
           className="border p-2 bg-white rounded-xl"
           value={todo}
-          onChange={(e) => setTodo(e.target.value)}
+          onChange={(e) => handleInputChange(e.target.value)}
         />
+        {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
         <button
           type="submit"
-          className="bg-amber-600 border rounded-xl block p-2 pr-10 pl-10 ml-10 mt-2 border-none hover:bg-amber-500 transition all 3s"
+          className="bg-amber-600 border rounded-xl block p-2 pr-10 pl-10 ml-10 mt-2 border-none hover:bg-amber-500 transition all 0.3s"
         >
           Add
         </button>
